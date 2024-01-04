f96b3373b2dd4c7270374b4964d758e0fa9ac238
Merar Mabrouk <119806956+Merar-Mabrouk@users.noreply.github.com>
2024-01-04 17:48:44 +0100

    next step

--- si/project/app1/models.py
+++ si/project/app1/models.py
@@ -2,9 +2,13 @@
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
-class Product(models.Model):
-    code = models.CharField(max_length=10, unique=True,auto_created=True)
-    designation = models.CharField(max_length=50,default=MATIERE_PREMIERE)
+# class Product(models.Model):
+#     code = models.CharField(max_length=10, unique=True,auto_created=True)
+#     designation = models.CharField(max_length=50,default=MATIERE_PREMIERE)
+    
+class RawMaterial(models.Model):
+    code = models.CharField(max_length=10, unique=True)
+    name = models.CharField(max_length=50)    
 
 class Client(models.Model):
     code = models.CharField(max_length=10, unique=True,auto_created=True)
@@ -35,22 +39,43 @@ class Employee(models.Model):
     daily_salary = models.FloatField()
     centre = models.ForeignKey(Centre, on_delete=models.CASCADE)
 
-class Purchase(models.Model):
+class Achat(models.Model):
+    pur=models.CharField(auto_created=True, max_length=10)
     supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey(RawMaterial, on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add=True)
     quantity = models.IntegerField()
     unit_price = models.FloatField()
-    total_price = models.FloatField()
 
 class Transfer(models.Model):
-    date = models.DateField(auto_now_add=True)
+    tr=models.CharField(auto_created=True, max_length=10)
+    date = models.DateField(auto_now_add=True,unique=True)
     centre = models.ForeignKey(Centre, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey(RawMaterial, on_delete=models.CASCADE)
     quantity = models.IntegerField()
 
 class Sale(models.Model):
+    sale=models.CharField(auto_created=True, max_length=10)
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    ### TODO: verify if this is correct
+    date = models.DateField(auto_now_add=True)
+    product = models.ForeignKey(RawMaterial, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     unit_price = models.FloatField()
-    total_price = models.FloatField()
+    
+    
+class Stock(models.Model):
+    code=models.CharField(max_length=10,unique=True,auto_created=True)
+    name_P=models.ForeignKey(RawMaterial,on_delete=models.CASCADE)
+    quantity=models.IntegerField()
+    def __str__(self):
+        return self.name
+    
+class ReglementAchat(models.Model):
+    code=models.CharField(max_length=10,unique=True,auto_created=True)
+    numA=models.ForeignKey(Achat)
+    rigli=models.BooleanField(default=False)
+    restant=models.FloatField(max_length=10,null=False)
+    
+    
+    
